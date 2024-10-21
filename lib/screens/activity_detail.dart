d6f8bc5d1f564e2729d1f853cb03e73e32a35afd
Dwi Aji Cahyono <dwiaji724@gmail.com>
2024-10-21 20:04:41 +0700

    first commit

---
+++ lib/screens/activity_detail.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ActivityDetail extends StatefulWidget {
+  final String idActivity;
+  const ActivityDetail({super.key, required this.idActivity});
+
+  @override
+  State<ActivityDetail> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<ActivityDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/homepage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String counter = '';
+
+  void _incrementCounter() {
+    setState(() {
+      counter = 'Hello World';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              counter,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
